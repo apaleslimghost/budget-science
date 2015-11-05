5aee4343dffd52baa09a557069d98586064c64d8
Matt Brennan <mattyb1000@gmail.com>
2015-11-05 21:49:20 +0000

    partition by distance from mean

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var tx = require('./db.json'); 
 var _ = require('lodash');
+var util = require('util');
 
-var grouped = _.groupBy(tx, t => t.payee.split(/\s/)[0]);
+var within_σ = (n, σ, μ) => x => (μ - n*σ) <= x && x <= (μ + n*σ);
+
+var grouped = _.groupBy(tx, t => t.payee.split(/\W/)[0]);
 var gauss = _.mapValues(grouped, group => {
 	var amts = _.pluck(group, 'amount');
 	var μ = _.sum(amts) / amts.length;
@@ -10,4 +13,10 @@ var gauss = _.mapValues(grouped, group => {
 	return {σ, μ, n: amts.length};
 });
 
-console.log(gauss);
+var outside = _.mapValues(grouped, (group, k) =>
+	_.partition(group, t =>
+		within_σ(2, gauss[k].σ, gauss[k].μ)(t.amount)
+	)
+);
+
+console.log(util.inspect(outside, {depth: null}));
