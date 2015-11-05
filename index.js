8c761ca8c0f91e3f7240c3c94b193d9a32e9efb1
Matt Brennan <mattyb1000@gmail.com>
2015-11-05 21:24:55 +0000

    do a gauss

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 var tx = require('./db.json'); 
 var _ = require('lodash');
 
-console.log(_.groupBy(tx, t => t.payee.split(/\W/)[0]));
+var grouped = _.groupBy(tx, t => t.payee.split(/\s/)[0]);
+var gauss = _.mapValues(grouped, group => {
+	var amts = _.pluck(group, 'amount');
+	var μ = _.sum(amts) / amts.length;
+	var d = _.map(amts, a => Math.pow(μ - a, 2));
+	var σ = Math.sqrt(_.sum(d) / amts.length);
+	return {σ, μ, n: amts.length};
+});
 
+console.log(gauss);
