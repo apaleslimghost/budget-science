1803ffe9bca62a0df0aa77b592b4172ae10993c2
Matt Brennan <mattyb1000@gmail.com>
2015-11-26 22:52:50 +0000

    attempt to recurring transactions

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ var gaussian = require('./gaussian');
 var group = require('./group');
 var nameGroups = require('./name-groups');
 var sortBy = require('lodash.sortby');
-var moment = require('moment');
 var pairs = xs => xs.length < 2 ? [] : [xs.slice(0, 2)].concat(pairs(xs.slice(1)));
+var sum = require('lodash.sum');
+
+var MONTH_MS = (365.2425 / 12) * 24 * 60 * 60 * 1000;
 
 function groupMeta(fn) {
 	return g => {
@@ -13,21 +15,23 @@ function groupMeta(fn) {
 	};
 }
 
-var groups = group(tx, {threshold: 2})
+var groups = group(tx, {threshold: 5})
 .map(groupMeta(function gauss(g) {
 	return gaussian(g.map(t => t.amount));
 }))
 .map(groupMeta(function timeGauss(g) {
-	if(g.length === 1) return;
+	if(g.length <= 2) return;
 	return gaussian(pairs(sortBy(g, t => new Date(t.date))).map(p => {
 		return new Date(p[1].date) - new Date(p[0].date);
 	}));
 }));
 
-var recurring = groups.filter(g => g.length > 2 && g.timeGauss.σ < g.timeGauss.μ / 10).map(g => {
-	g.timeGauss.σ = moment.duration(g.timeGauss.σ).humanize();
-	g.timeGauss.μ = moment.duration(g.timeGauss.μ).humanize();
-	return g;
-});
+var recurring = groups
+.filter(g => g.length > 2 && g.timeGauss.σ < g.timeGauss.μ / 10)
+.map(groupMeta(function perMonth(g) {
+	return MONTH_MS * g.gauss.μ / g.timeGauss.μ;
+}));
 
 console.log(nameGroups(recurring));
+console.log(Object.keys(nameGroups(groups)));
+console.log(sum(recurring, 'perMonth'));
