47bc5dd6db7c8678ce880a433c8029023c92d407
Matt Brennan <mattyb1000@gmail.com>
2015-11-26 22:53:00 +0000

    configurable similarity

--- similarity.js
+++ similarity.js
@@ -1,8 +1,11 @@
 var levenshtein = require('fast-levenshtein').get;
 
-module.exports = function similarity(t1, t2) {
-	var amtSimilar = Math.pow(t1.amount - t2.amount, 2);
-	var payeeSimilar = levenshtein(t1.payee, t2.payee) / 5;
+module.exports = function similarity(t1, t2, options) {
+	options = options || {};
+	var payeeWeight  = options.payeeWeight  || 3;
+	var amountWeight = options.amountWeight || 1;
+	var amtSimilar = Math.pow(t1.amount - t2.amount, 2) / amountWeight;
+	var payeeSimilar = levenshtein(t1.payee, t2.payee) / payeeWeight;
 
 	return Math.pow(amtSimilar + payeeSimilar, 2);
 };
