db5a38358c68ea1788073a7a3c95a226cacdd5a5
Matt Brennan <mattyb1000@gmail.com>
2015-11-26 22:52:13 +0000

    nicer category names

--- name-group.js
+++ name-group.js
@@ -1,9 +1,13 @@
-var prefix = require('common-prefix');
+var prefix = require('common-prefix')
+var titleCase = require('title-case');
+
+var normaliseWhitespace = s => s.replace(/\s+/g, ' ').trim();
 
 module.exports = function nameGroup(group, names) {
-	var name = prefix(group.map(t => t.payee));
+	var payees = group.map(t => normaliseWhitespace(titleCase(t.payee)));
+	var name = normaliseWhitespace(prefix(payees) || payees.join(', '));
 	var suffixed = name;
 	var i = 0;
-	while(~names.indexOf(suffixed)) suffixed = name + '-' + (++i);
+	while(~names.indexOf(suffixed)) suffixed = name + ' (' + (++i) + ')';
 	return suffixed;
 };
