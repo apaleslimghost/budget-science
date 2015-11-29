a4e258296e7ed05dedc3e1dc48dc8a2f7cca5251
Matt Brennan <mattyb1000@gmail.com>
2015-11-29 14:25:19 +0000

    group as class with getters

--- src/group.js
+++ src/group.js
@@ -1,26 +1,99 @@
+var sortBy = require('lodash.sortby');
+var moment = require('moment');
+
+var nameGroup = require('./name-group');
 var similarity = require('./similarity');
+var gaussian = require('./gaussian');
 
-module.exports = function group(tx, options) {
-	options = options || {};
-	var groups = options.groups || [];
-	var threshold = options.threshold || 1;
-	var skip = options.skip || [];
+var pairs = xs => xs.length < 2 ? [] : [xs.slice(0, 2)].concat(pairs(xs.slice(1)));
 
-	tx.forEach((t1, i) => {
-		if(~skip.indexOf(t1.hash)) return;
-		var group = [t1];
-		groups.push(group);
-		var k = groups.length;
+const MONTH_MS = (365.2425 / 12) * 24 * 60 * 60 * 1000;
+var sixMonthsAgo = moment().subtract(6, 'months');
 
-		tx.slice(i + 1).forEach(t2 => {
-			if(~skip.indexOf(t2.hash)) return;
-			if(similarity(t1, t2, options) < threshold) {
-				group.push(t2);
-				skip.push(t2.hash);
-			}
+module.exports = class Group {
+	static from(t) {
+		if(t instanceof Group) return t;
+		if(t.transactions) return new Group(t.transactions);
+		if(Array.isArray(t)) return new Group(t);
+		throw new Error(`Can't convert ${t} to Group`);
+	}
+
+	static groupTransactions(tx, options) {
+		options = options || {};
+		var groups = options.groups.map(Group.from) || [];
+		var threshold = options.threshold || 1;
+		var skip = options.skip || [];
+
+		tx.forEach((t1, i) => {
+			if(~skip.indexOf(t1.hash)) return;
+			var group = new Group([t1]);
+			groups.push(group);
+			var k = groups.length;
+
+			tx.slice(i + 1).forEach(t2 => {
+				if(~skip.indexOf(t2.hash)) return;
+				if(similarity(t1, t2, options) < threshold) {
+					group.add(t2);
+					skip.push(t2.hash);
+				}
+			});
 		});
-	});
 
-	return groups;
-};
+		return groups;
+	}
 
+	constructor(transactions) {
+		this.transactions = transactions;
+	}
+
+	add(transaction) {
+		this.transactions.push(transaction);
+		return this;
+	}
+
+	get gaussian() {
+		return gaussian(this.transactions.map(t => t.amount));
+	}
+
+	get timeGaussian() {
+		if(this.length <= 2) return {};
+		return gaussian(pairs(sortBy(this.transactions, t => new Date(t.date))).map(([t1, t2]) => {
+			return new Date(t2.date) - new Date(t1.date);
+		}));
+	}
+
+	get recurring() {
+		var expectedIn6Months = 6 * MONTH_MS / this.timeGaussian.μ;
+		var actualIn6Months = this.transactions.filter(t => moment(t.date).isAfter(sixMonthsAgo)).length;
+
+		return this.length > 2
+		    && this.timeGaussian.σ < this.timeGaussian.μ / 10
+		    && Math.pow(1 - actualIn6Months / expectedIn6Months, 2) < 0.1;
+	}
+
+	get perMonth() {
+		return MONTH_MS * this.gaussian.μ / this.timeGaussian.μ;
+	}
+
+	get name() {
+		return nameGroup(this.transactions);
+	}
+
+	get length() {
+		return this.transactions.length;
+	}
+
+	toString() {
+		return this.name;
+	}
+
+	toJSON() {
+		return {
+			transactions: this.transactions,
+			gaussian:     this.gaussian,
+			timeGaussian: this.timeGaussian,
+			recurring:    this.recurring,
+			perMonth:     this.perMonth,
+		};
+	}
+}
--- src/index.js
+++ src/index.js
@@ -1,28 +1,11 @@
 var tx = require('../db.json'); 
-var gaussian = require('./gaussian');
-var group = require('./group');
+var Group = require('./group');
 var nameGroups = require('./name-groups');
-var nameGroup = require('./name-group');
-var sortBy = require('lodash.sortby');
-var pairs = xs => xs.length < 2 ? [] : [xs.slice(0, 2)].concat(pairs(xs.slice(1)));
 var sum = require('lodash.sum');
 var flatten = require('lodash.flatten');
-var moment = require('moment');
 var fs = require('fs');
 
-var MONTH_MS = (365.2425 / 12) * 24 * 60 * 60 * 1000;
 
-function groupMeta(fn) {
-	return g => {
-		g[fn.name] = fn(g)
-		return g;
-	};
-}
-
-var lastTxDate = moment(Math.max.apply(null, tx.map(t => new Date(t.date))));
-var sixMonthsAgo = lastTxDate.clone().subtract(6, 'months');
-
-console.log(lastTxDate.toDate(), sixMonthsAgo.toDate());
 
 var existingGroups = [];
 try {
@@ -34,34 +17,13 @@ try {
 	payeesAlias = require('../payees.json');
 } catch(e) {}
 
-var groups = group(tx, {
+var groups = Group.groupTransactions(tx, {
 	threshold: 5,
 	groups: existingGroups,
-	skip:   flatten(existingGroups.map(g => g.map(t => t.hash))),
+	skip:   flatten(existingGroups.map(g => g.transactions.map(t => t.hash))),
 	payeesAlias: payeesAlias
-})
-.map(groupMeta(function gauss(g) {
-	return gaussian(g.map(t => t.amount));
-}))
-.map(groupMeta(function timeGauss(g) {
-	if(g.length <= 2) return {};
-	return gaussian(pairs(sortBy(g, t => new Date(t.date))).map(p => {
-		return new Date(p[1].date) - new Date(p[0].date);
-	}));
-}))
-.map(groupMeta(function recurring(g) {
-	var expectedIn6Months = 6 * MONTH_MS / g.timeGauss.μ;
-	var actualIn6Months = g.filter(t => moment(t.date).isAfter(sixMonthsAgo)).length;
+});
 
-	return g.length > 2
-	    && g.timeGauss.σ < g.timeGauss.μ / 10
-	    && Math.pow(1 - actualIn6Months / expectedIn6Months, 2) < 0.1;
-}))
-.map(groupMeta(function perMonth(g) {
-	return MONTH_MS * g.gauss.μ / g.timeGauss.μ;
-}));
-
-console.log(nameGroups(groups));
 console.log(sum(groups.filter(g => g.recurring), 'perMonth'));
 
 fs.writeFile('groups.json', JSON.stringify(groups, null, 2), 'utf8');
