340fecb009748bce33e4e41931c225565b5de22a
Matt Brennan <mattyb1000@gmail.com>
2016-03-12 21:02:15 +0000

    better payee alias structure

--- src/similarity.js
+++ src/similarity.js
@@ -1,24 +1,25 @@
 var levenshtein = require('fast-levenshtein').get;
 var defaults = require('lodash.defaults');
+var includes = require('lodash.includes');
 
 var normaliseWhitespace = require('./normalise');
 
 var absMax = (a, b) => (Math.abs(a) > Math.abs(b)) ? a : b;
+var aliased = (n1, n2, aliases) => aliases.some(alias => includes(alias, n1) && includes(alias, n2));
 
 module.exports = function similarity(t1, t2, options) {
 	var {payeeWeight, amountWeight, payeesAlias} = defaults(options, {
 		payeeWeight:  3,
 		amountWeight: 1,
-		payeesAlias:  {}
+		payeesAlias:  []
 	});
 
 	var amtSimilar = Math.pow((t1.amount - t2.amount) / absMax(t1.amount, t2.amount), 2) / amountWeight;
 	var payeeSimilar = levenshtein(t1.payee, t2.payee) / payeeWeight;
 
-	if(
-		payeesAlias[normaliseWhitespace(t1.payee)] === normaliseWhitespace(t2.payee) ||
-		payeesAlias[normaliseWhitespace(t2.payee)] === normaliseWhitespace(t1.payee)
-	) payeeSimilar = 0;
+	if(aliased(normaliseWhitespace(t1.payee), normaliseWhitespace(t2.payee), options.payeesAlias)) {
+		payeeSimilar = 0;
+	}
 
 	return Math.pow(amtSimilar + payeeSimilar, 2);
 };
